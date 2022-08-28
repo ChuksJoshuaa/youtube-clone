cf41ea5d8222fcd9008b296565c6e2a854a8adbb
D-coder <chuksjoshuaa@gmail.com>
2022-08-28 16:08:19 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ChannelDetail, Navbar, Feed, SearchFeed, VideoDetail } from "./components"
+import { Box } from "@mui/material"
+
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000"}}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Index.jsx
@@ -0,0 +1,11 @@
+import Feed from "./Feed"
+import VideoDetail from "./VideoDetail"
+import ChannelDetail from "./ChannelDetail"
+import Navbar from "./Navbar"
+import SearchFeed from "./SearchFeed"
+import SearchBar from "./SearchBar"
+import Sidebar from "./Sidebar"
+import Videos from "./Videos"
+
+
+export {Feed, VideoDetail, ChannelDetail, Navbar, SearchFeed, SearchBar, Sidebar, Videos}
