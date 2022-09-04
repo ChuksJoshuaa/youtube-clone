a9fbd242dffdf60921f619c25653e9398eb92c35
D-coder <chuksjoshuaa@gmail.com>
2022-09-04 15:58:52 +0100

    Changes made to Videos

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -2,10 +2,28 @@ import React, { useEffect } from 'react'
 import { Link, useParams } from "react-router-dom"
 import { Typography, Stack, Box } from '@mui/material'
 import ReactPlayer from "react-player"
+import { CheckCircle } from "@mui/icons-material"
+import { Video } from "./Index"
+import { fetchFromAPI } from '../utils/fetchFromApi'
 
 const VideoDetail = () => {
+  const { id } = useParams()
+  const [videoDetail, setVideoDetail] = useState(null)
+
+  useEffect(() => {
+   fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+   .then((data) => setVideoDetail(data?.items[0]))
+  }, [])
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px"}}>
+            <ReactPlayer />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
